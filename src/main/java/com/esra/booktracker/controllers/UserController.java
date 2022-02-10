7bf694b4b91aa2f939022fa521887ccaf7479ce6
Esra Yazar <esrayzr@gmail.com>
2022-02-09 22:45:53 -0500

    Bugs are fixed

--- src/main/java/com/esra/booktracker/controllers/UserController.java
+++ src/main/java/com/esra/booktracker/controllers/UserController.java
@@ -5,9 +5,11 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -22,12 +24,27 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	@Autowired
-	private UserValidator validator;
+	private UserValidator userValidator;
 
 	@GetMapping("/")
-	public String login(@ModelAttribute("user") User user) {
+	public String welcome(@ModelAttribute("user") User user) {
 		return "registration.jsp";
 	}
+	@GetMapping("/dashboard")
+	public String dashboard(Model model, HttpSession session) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
+		model.addAttribute("user", user);
+		
+		return "dashboard.jsp";
+	}
+	@GetMapping("/register")
+	public String getRegister(@ModelAttribute("user") User user) {
+		return "registration.jsp";
+	}
+	
+
 
 	/**
 	 * User registration
@@ -39,14 +56,20 @@ public class UserController {
 	@PostMapping("/register")
 	public String register(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session) {
 		System.out.println("User:" + user.toString());
-		validator.validate(user, result);
+		userValidator.validate(user, result);
 		if (result.hasErrors()) {
 			return "registration.jsp";
 		}
 		User newUser = this.userService.registerUser(user);
 		session.setAttribute("user__id", newUser.getId());
-		return "redirect:/ideas/dashboard";
+		return "redirect:/dashboard";
 	}
+	
+	@GetMapping("/login")
+	public String login(@ModelAttribute("user") User user) {
+		return "login.jsp";
+	}
+
 
 	/**
 	 * Handle User login
@@ -65,7 +88,7 @@ public class UserController {
 		}
 		User userToBeLoggedIn = this.userService.getUserByEmail(email);
 		session.setAttribute("user__id", userToBeLoggedIn.getId());
-		return "redirect:/ideas/dashboard";
+		return "redirect:/dashboard";
 	}
 
 	/**
@@ -78,6 +101,44 @@ public class UserController {
 		session.invalidate();
 		return "redirect:/";
 	}
+	
+	@GetMapping("/profile/{id}")
+	public String userProfile(HttpSession session, Model viewModel, @ModelAttribute("user") User user) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User currentUser = this.userService.findOneUser((Long) session.getAttribute("user__id"));
+		viewModel.addAttribute("user", currentUser);
+		return "userprofile.jsp";
+	}
+	@GetMapping("/user/edit/{id}")
+	public String edit(@PathVariable("id") Long id, @ModelAttribute("user") User user, Model model,
+			HttpSession session) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User editUser = userService.findOneUser(id);
+		// Check if the given id returns anything from DB
+		// This will prevent white label error on UI
+		if (editUser == null) {
+			System.out.println("Idea id=" + id + " is not found in DB.");
+			return "redirect:/ideas/dashboard";
+		}
+		// Verify User has access to Edit the User
+		if (editUser.getId().compareTo((Long) session.getAttribute("user__id")) != 0)
+			return "redirect:/ideas/dashboard";
+		model.addAttribute("user", editUser);
+		return "edituser.jsp";
+	}
+	@PostMapping("user/update/{id}")
+	public String update(@PathVariable("id") Long id, @Valid @ModelAttribute("user") User user,
+			BindingResult result, HttpSession session, Model model) {
+		userValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "edituser.jsp";
+		}
+		userService.updateUser(id, user);
+		return "redirect:userprofile.jsp";
+	}
+	
 
 }
 
