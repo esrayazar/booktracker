d766fb7510f286267cb53902662fa97f66607a5b
Esra Yazar <esrayzr@gmail.com>
2022-02-10 21:53:23 -0500

    bugs fixed

--- src/main/java/com/esra/booktracker/validators/UserValidator.java
+++ src/main/java/com/esra/booktracker/validators/UserValidator.java
@@ -1,19 +1,14 @@
 package com.esra.booktracker.validators;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 
 import com.esra.booktracker.models.User;
-import com.esra.booktracker.repositories.UserRepository;
-
-
 
 @Component
 public class UserValidator {
-	@Autowired
 
-	private UserRepository uRepo;
 	
 	public boolean supports(Class<?> clazz) {
 		return User.class.equals(clazz);
@@ -21,16 +16,17 @@ public class UserValidator {
 	
 	public void validate(Object target, Errors errors) {
 		User user =(User) target;
-		if(!user.getPassword().equals(user.getConfirmPassword())) {
-			errors.rejectValue("password", "Match", "Password do not match!!!!!!!!");
-		}
-		if (this.uRepo.existsByEmail(user.getEmail())) {
-			errors.rejectValue("email", "Unique", "Email has already taken");
-		}
-		if (user.getFirstName().equals("reena")) {
-			errors.rejectValue("firstName", "noMattAllowed", "Reena s  are unable to register at this time :)");
-		}
 		
+		if (user.getFirstName()==null ||
+				user.getFirstName().isEmpty()) {
+		System.out.println("1..");
+			errors.rejectValue("firstName", "noMattAllowed", "First name shouldnt be empty");
+		}
+		if (user.getLastName()==null ||
+				user.getLastName().isEmpty()) {
+			errors.rejectValue("lastName", "noMattAllowed", "Last name shouldnt be empty");
+			System.out.println("2..");
+		}
 	}
 
 }
