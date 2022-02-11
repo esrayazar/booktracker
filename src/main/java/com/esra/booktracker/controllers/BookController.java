d123025b49a439178128ac0c6fed73fc3330932f
Esra Yazar <esrayzr@gmail.com>
2022-02-10 21:49:45 -0500

    wishlist, like,unlike funct added

--- src/main/java/com/esra/booktracker/controllers/BookController.java
+++ src/main/java/com/esra/booktracker/controllers/BookController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.esra.booktracker.models.Book;
 import com.esra.booktracker.models.Rating;
@@ -21,9 +22,14 @@ import com.esra.booktracker.services.UserService;
 import com.esra.booktracker.validators.BookValidator;
 
 
+
 @Controller
 @RequestMapping("/books")
 public class BookController {
+	
+	private static final String PROFILE_PAGE = "bookprofile";
+	private static final String WISHLIST_PAGE = "wishlist";
+	private static final String COMPLETED_PAGE = "completedlist";
 	
 	@Autowired
 	private UserService userService;
@@ -32,6 +38,18 @@ public class BookController {
 	@Autowired
 	private BookValidator bookValidator;
 	
+	@GetMapping("/{id}")
+	public String bookProfile(@PathVariable("id") Long id,HttpSession session, Model viewModel) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
+		viewModel.addAttribute("user", user);
+		Book book = bookService.getBookById(id);
+		if(book!= null ) {
+			viewModel.addAttribute("book", book);
+		}
+		return "bookprofile.jsp";
+	}
 	@GetMapping("/create")
 	public String newBook(HttpSession session, Model viewModel, @ModelAttribute("book") Book book) {
 		// Check if there is any active user session.
@@ -49,11 +67,17 @@ public class BookController {
 		bookValidator.validate(book, result);
 		if (result.hasErrors()) {
 			viewModel.addAttribute("user", user);
-			return "new.jsp";
+			return "newbook.jsp";
 		}
 		book.setUser(user);
-		this.bookService.create(book);
-		return "redirect:/dashboard";
+		try {
+			this.bookService.create(book);
+			return "redirect:/dashboard";
+		}catch(Exception e){
+			viewModel.addAttribute("error", "Book is already exist in the database with ISBN= " + book.getIsbn());
+		}
+		viewModel.addAttribute("user", user);
+		return "newbook.jsp";
 	}
 	
 	@GetMapping("/edit/{id}")
@@ -99,13 +123,85 @@ public class BookController {
 		this.bookService.deleteBook(id);
 		return "redirect:/books/dashboard";
 	}
+	@GetMapping("/search")
+	public String search(@RequestParam String term,HttpSession session, Model viewModel, @ModelAttribute("book") Book book) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
+		viewModel.addAttribute("user", user);
+		viewModel.addAttribute("books", bookService.searchBook(term));
+		return "searchpage.jsp";
+	}
+	//Like
+	@GetMapping("/{id}/like/{page}")
+	public String like(HttpSession session, @PathVariable("id") Long id, @PathVariable("page") String page) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
+		Book book = this.bookService.getOneBook(id);
+		this.bookService.likeBook(user, book);
+		return whichPage(page);
+	}
+	
+	
+	//Unlike
+	@GetMapping("/{id}/unlike/{page}")
+	public String unlike(HttpSession session, @PathVariable("id") Long id, @PathVariable("page") String page) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
+		Book book = this.bookService.getOneBook(id);
+		this.bookService.unlikeBook(user, book);
+		return whichPage(page);
+	}
+	//Add WishList
+	
+	@GetMapping("/{id}/wish/add/{page}")
+	public String addWishList(HttpSession session, @PathVariable("id") Long id, @PathVariable("page") String page) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
+		Book book = this.bookService.getOneBook(id);
+		this.bookService.addWishList(user, book);
+		return whichPage(page);
+	}
+	
+	//Remove WishList
+	@GetMapping("/{id}/wish/remove/{page}")
+	public String removeWhishList(HttpSession session, @PathVariable("id") Long id, @PathVariable("page") String page) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
+		Book book = this.bookService.getOneBook(id);
+		this.bookService.removeWishList(user, book);
+		return whichPage(page);
+	}
+	
+	//Complete Reading a Book
+	@GetMapping("/complete")
+	public String completeBook(HttpSession session, @RequestParam String isbn) {
+		// Check if there is any active user session.
+		if(session.getAttribute("user__id") == null) return "redirect:/";
+		User user = this.userService.findOneUser((Long) session.getAttribute("user__id"));
+		Book book = this.bookService.searchBookByIsbn(isbn);
+		//this.bookService.completedBookRead(user, book);
+		return "redirect:/dashboard";
+	}
 	
 	//Rating
 	@PostMapping("/addRating")
 	public String addRating(@ModelAttribute("newRating") Rating rating) {
 		bookService.AddRating(rating);
-		return "redirect:/arts";
+		return "redirect:/books";
 		
+	}
+	
+	private String whichPage(String page) {
+		if(page.equals(PROFILE_PAGE))
+			return "redirect:/books/{id}";
+		else if(page.equals(WISHLIST_PAGE))
+			return "redirect:/wishlist";
+		return "redirect:/";
 	}
 
 	
