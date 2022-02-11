0192cdef005efb0c82dda3997099c8c80bc3d1ea
Esra Yazar <esrayzr@gmail.com>
2022-02-10 21:52:14 -0500

    query added

--- src/main/java/com/esra/booktracker/repositories/BookRepository.java
+++ src/main/java/com/esra/booktracker/repositories/BookRepository.java
@@ -2,8 +2,9 @@ package com.esra.booktracker.repositories;
 
 import java.util.List;
 
-
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.esra.booktracker.models.Book;
@@ -12,7 +13,13 @@ import com.esra.booktracker.models.Book;
 @Repository
 public interface BookRepository extends CrudRepository<Book, Long> {
 	List<Book> findAll();
+	Book findByIsbn(String isbn);
 
+	@Query(
+		value = "SELECT * FROM books b WHERE b.title LIKE %:term% OR b.author LIKE %:term% OR b.genre LIKE %:term% OR b.year LIKE %:term%", 
+		nativeQuery = true)
+	List<Book> findAllbySearchTerm(@Param("term") String term);
+//where b.title like %:term%
 //	@Query("SELECT i FROM books i Order By i.likers.size DESC")
 //	List<Book> findAllByLikersOrderByLikersDesc();
 //
