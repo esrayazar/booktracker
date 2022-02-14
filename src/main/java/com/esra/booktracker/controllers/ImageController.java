2ca2651ebea02393c196ccc856ca382ce9ae0ffb
Esra Yazar <esrayzr@gmail.com>
2022-02-13 23:26:21 -0500

    image controller created

---
+++ src/main/java/com/esra/booktracker/controllers/ImageController.java
@@ -0,0 +1,68 @@
+package com.esra.booktracker.controllers;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.esra.booktracker.models.Book;
+import com.esra.booktracker.models.Image;
+import com.esra.booktracker.repositories.ImageRepository;
+import com.esra.booktracker.services.BookService;
+import com.esra.booktracker.utility.ImageUtility;
+
+@RestController
+//@CrossOrigin(origins = "http://localhost:8082") open for specific port
+@CrossOrigin() // open for all ports
+public class ImageController {
+
+    @Autowired
+    ImageRepository imageRepository;
+    @Autowired
+    BookService bookService;
+
+    @GetMapping(path = {"/get/image/info/{name}"})
+    public Image getImageDetails(@PathVariable("name") String name) throws IOException {
+
+        final Optional<Image> dbImage = imageRepository.findByName(name);
+
+        return Image.builder()
+                .name(dbImage.get().getName())
+                .type(dbImage.get().getType())
+                .image(ImageUtility.decompressImage(dbImage.get().getImage())).build();
+    }
+
+    @GetMapping(path = {"/get/image/byname/{name}"})
+    public ResponseEntity<byte[]> getImage(@PathVariable("name") String name) throws IOException {
+
+        final Optional<Image> dbImage = imageRepository.findByName(name);
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.valueOf(dbImage.get().getType()))
+                .body(ImageUtility.decompressImage(dbImage.get().getImage()));
+    }
+    
+    @GetMapping(path = {"/get/image/byid/{id}"})
+    public ResponseEntity<byte[]> getImagebyId(@PathVariable("id") Long id) throws IOException {
+
+        final Optional<Image> dbImage = imageRepository.findById(id);
+
+        return ResponseEntity
+                .ok()
+                .contentType(MediaType.valueOf(dbImage.get().getType()))
+                .body(ImageUtility.decompressImage(dbImage.get().getImage()));
+    }
+}
