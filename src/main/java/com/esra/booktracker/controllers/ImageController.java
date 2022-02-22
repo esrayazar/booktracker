a6a258e08af11c0b53f910ce105d8213815eb09f
Esra Yazar <esrayzr@gmail.com>
2022-02-21 21:00:31 -0500

    bugs fixed

--- src/main/java/com/esra/booktracker/controllers/ImageController.java
+++ src/main/java/com/esra/booktracker/controllers/ImageController.java
@@ -3,21 +3,14 @@ package com.esra.booktracker.controllers;
 import java.io.IOException;
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.esra.booktracker.models.Book;
+import org.springframework.web.bind.annotation.RestController;
 import com.esra.booktracker.models.Image;
 import com.esra.booktracker.repositories.ImageRepository;
 import com.esra.booktracker.services.BookService;
